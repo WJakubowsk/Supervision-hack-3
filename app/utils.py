1ddf1970c424350c6d14b3e0519bd652f4aa1f23
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-11-18 09:49:00 +0100

    add docstrings

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,9 @@ import streamlit as st
 from typing import List
 
 def file_selector(folder_path: str = '.', select_all: bool = False) -> List[str]:
+    """
+    Module for selecting files from the given local path.
+    """
     filenames = os.listdir(folder_path)
     if select_all:
         selected_files = st.multiselect('Select files', filenames, default=filenames)
