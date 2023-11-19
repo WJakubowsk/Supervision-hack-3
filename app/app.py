420463598dd059fb69cae26114628845f0bce072
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-11-19 05:49:21 +0100

    add difference show between versions

--- app/app.py
+++ app/app.py
@@ -17,7 +17,29 @@ def main():
     st.write('### Selected files:')
     st.write(selected_files)
 
-    # Tu można dodać kod do analizy lub porównania wybranych plików
+    # podziel na sekcje
+
+    # sprawdz kompletnosc sekcji
+
+    # wyciagnij tabele z pliku
+
+    if st.checkbox('Compare Texts from two files:'):
+        try:
+            # Input for file paths
+            uploaded_file1 = st.file_uploader('Upload first file:', type=['pdf'])
+            uploaded_file2 = st.file_uploader('Upload second file:', type=['pdf'])
+
+            text_file1 = extract_text_from_pdf(uploaded_file1)
+            text_file2 = extract_text_from_pdf(uploaded_file2)
+
+            differences = compare_strings(text_file1, text_file2)
+            colored_lines = [color_line(line) for line in differences]
+            st.markdown(f'Estimated similarity between two files: {calculate_similarity_ratio_between_strings(text_file1, text_file2)}%.')
+            st.markdown(f'### Differences:')
+            for line in colored_lines:
+                st.markdown(line, unsafe_allow_html=True)
+        except Exception as e:
+            st.markdown('Waiting for input..')
 
 if __name__ == '__main__':
     main()
